30f32e59c877b0a5b127ec74803497b5b91f6009
basi743 <146139441+basi743@users.noreply.github.com>
2025-04-14 12:01:18 +0200

    Update index.js

--- api/get-user/index.js
+++ api/get-user/index.js
@@ -1,8 +1,11 @@
 const admin = require('firebase-admin');
 
+// Load service account from environment variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(require('../../serviceAccountKey.json')),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
