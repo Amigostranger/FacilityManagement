2e0672505833ff8d1b6c5ac364f3d82ac0b5ef38
basi743 <basicodes743@gmail.com>
2025-04-22 21:47:46 +0200

    changed jest.confg.js

--- public/js/jest.config.js
+++ public/js/jest.config.js
@@ -1,4 +1,11 @@
-export default {
+module.exports = {
   transform: {},
   testEnvironment: "jsdom",
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "public/js/**/*.js",
+    "!public/js/tests/**"
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov"]
 };
