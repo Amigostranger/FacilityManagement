1ca2a74d9e6fd940bed8d94cbfa8274fcf376789
Jan Moloto <janmoloto8@gmail.com>
2025-04-29 20:58:02 +0200

    Modify files for Events

--- server.js
+++ server.js
@@ -12,17 +12,18 @@ console.log('Server is starting');
 
 
 
-// const serviceAccountPath = path.resolve('./serviceAccountKey.json');
+const serviceAccountPath = path.resolve('./serviceAccountKey.json');
 
-// if (!fs.existsSync(serviceAccountPath)) {
-//   console.error(`serviceAccountKey.json not found at ${serviceAccountPath}`);
-//   process.exit(1);
-// }
+if (!fs.existsSync(serviceAccountPath)) {
+  console.error(`serviceAccountKey.json not found at ${serviceAccountPath}`);
+  process.exit(1);
+}
 
-// const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
 
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
 
 // Initialize Firebase Admin SDK with the service account credentials
 admin.initializeApp({
@@ -78,6 +79,35 @@ const verifyToken = async (req, res, next) => {
   }
 };
 
+//API Endpoint for creating an event
+app.post("/api/createEvent", verifyToken,async (req,res) => {
+  const {title, description, facility, date, start, end, who}=req.body 
+  const uid=req.user.uid;
+  if (!title || !description || !facility || !start || !end || !who) {
+    return res.status(400).json({ error: "All fields required" });
+  }
+
+  try {
+    await db.collection("bookings").add({
+      title,
+      description,
+      facility,
+      submittedBy: uid,
+      date,
+      start,
+      end,
+      who,
+      //createdAt: new Date(),
+    });
+
+    res.status(200).json({ message: "Report submitted" });
+  }
+  catch{
+    console.error("Report save error:", error);
+    res.status(500).json({ error: "Failed to save event" });
+}
+
+});
 
 app.get("/api/notifications", verifyToken,async (req, res) => {
   
