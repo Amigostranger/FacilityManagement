09b6ed9cb64db68528fa477d855ba469812b39eb
Jan Moloto <janmoloto8@gmail.com>
2025-05-04 22:23:36 +0200

    FINAL

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,26 +1,23 @@
 // jest.config.mjs
 export default {
-    collectCoverage: true,
-    collectCoverageFrom: [
-      'public/js/**/*.js',
-      '!public/js/tests/**',
-      '!public/js/firebase.js'
-    ],
-    coverageDirectory: 'coverage',
-    testEnvironment: 'jsdom',
-  
-    // Use babel-jest to transform modern ESM code
-    transform: {
-      '^.+\\.js$': ['babel-jest', { presets: ['@babel/preset-env'] }]
-    },
-  
-    transformIgnorePatterns: [
-      '/node_modules/(?!firebase)/'
-    ],
-  
-    extensionsToTreatAsEsm: ['.js'], // <-- This tells Jest to treat .js files as ESM
-  
-    // Optional: silence experimental warnings
-    moduleFileExtensions: ['js', 'json', 'jsx', 'node'],
-  };
-  
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'public/js/**/*.js',
+    '!public/js/tests/**',
+    '!public/js/firebase.js'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+
+  // Use babel-jest to transform modern ESM code
+  transform: {
+    '^.+\\.js$': ['babel-jest', { presets: ['@babel/preset-env'] }]
+  },
+
+  transformIgnorePatterns: [
+    '/node_modules/(?!firebase)/'
+  ],
+
+  // Optional: silence experimental warnings
+  moduleFileExtensions: ['js', 'json', 'jsx', 'node'],
+};
