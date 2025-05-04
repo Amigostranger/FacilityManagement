ab02b65b06e6c290af2013640d0588de707132f9
Jan Moloto <janmoloto8@gmail.com>
2025-05-04 21:36:11 +0200

    Modify babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,12 @@
 //     "presets"; ["@babel/preset-env"]
 //   }
 // babel.config.js
-export default {
+// export default {
+//   presets: ['@babel/preset-env'],
+// };
+
+// babel.config.js
+module.exports = {
   presets: ['@babel/preset-env'],
+  // add other config as needed
 };
