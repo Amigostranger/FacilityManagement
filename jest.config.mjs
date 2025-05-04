b580099a09c20d03e37dadca6a65d10421828472
Jan Moloto <janmoloto8@gmail.com>
2025-05-04 22:00:31 +0200

    Fix firestore imports

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,18 +1,26 @@
+// jest.config.mjs
 export default {
-  collectCoverage: true,
-  collectCoverageFrom: [
-    'public/js/tests/**/*.js' // ✅ Only collect from tests folder
-  ],
-  coverageDirectory: 'coverage',
-  testEnvironment: 'jsdom',
-
-  transform: {
-    '^.+\\.js$': ['babel-jest', { presets: ['@babel/preset-env'] }]
-  },
-
-  transformIgnorePatterns: [
-    '/node_modules/(?!firebase)/'
-  ],
-
-  moduleFileExtensions: ['js', 'json', 'jsx', 'node'],
-};
+    collectCoverage: true,
+    collectCoverageFrom: [
+      'public/js/**/*.js',
+      '!public/js/tests/**',
+      '!public/js/firebase.js'
+    ],
+    coverageDirectory: 'coverage',
+    testEnvironment: 'jsdom',
+  
+    // Use babel-jest to transform modern ESM code
+    transform: {
+      '^.+\\.js$': ['babel-jest', { presets: ['@babel/preset-env'] }]
+    },
+  
+    transformIgnorePatterns: [
+      '/node_modules/(?!firebase)/'
+    ],
+  
+    extensionsToTreatAsEsm: ['.js'], // <-- This tells Jest to treat .js files as ESM
+  
+    // Optional: silence experimental warnings
+    moduleFileExtensions: ['js', 'json', 'jsx', 'node'],
+  };
+  
