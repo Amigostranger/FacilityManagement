1535920453243a8dad20bda29a01f7f6b766123c
strangerhm3@gmail.com <strangerhm3@gmail.com>
2025-05-05 14:59:39 +0200

    all working

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js"; // Same version as firebase-auth
 // // import { initializeApp } from 'firebase/app';
 
