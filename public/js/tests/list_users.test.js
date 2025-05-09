2a3e4463484918e35be12015e70ed6621eb9910f
Jan Moloto <164632870+JanMoloto8@users.noreply.github.com>
2025-05-09 15:54:15 +0200

    Update list_users.test.js

--- public/js/tests/list_users.test.js
+++ public/js/tests/list_users.test.js
@@ -5,6 +5,16 @@ const { loadUsers, talkToit } = require('./list_users');
 
 global.fetch = jest.fn();
 
+beforeAll(() => {
+  jest.spyOn(console, 'error').mockImplementation(() => {});
+  jest.spyOn(console, 'log').mockImplementation(() => {});
+});
+
+afterAll(() => {
+  console.error.mockRestore();
+  console.log.mockRestore();
+});
+
 describe('talkToit', () => {
   let event;
 
