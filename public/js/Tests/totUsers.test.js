d979f66631f9084766f684a1844ee0dac617af2f
strangerhm3@gmail.com <strangerhm3@gmail.com>
2025-05-18 23:14:56 +0200

    tot_users.js tested

--- public/js/Tests/totUsers.test.js
+++ public/js/Tests/totUsers.test.js
@@ -1,47 +1,4 @@
-// import { getTotalUsers } from '../tot_users'; // Adjust this path
-// import fetch from 'node-fetch';
 
-// jest.mock('node-fetch', () => jest.fn());
-
-// global.fetch = fetch;
-
-// describe('getTotalUsers', () => {
-//   afterEach(() => {
-//     jest.clearAllMocks();
-//   });
-
-//   it('sets totalUsers to correct length on success', async () => {
-//     const mockUsers = [{}, {}, {}, {}]; // 4 users
-
-//     fetch.mockResolvedValueOnce({
-//       ok: true,
-//       json: jest.fn().mockResolvedValueOnce(mockUsers),
-//     });
-
-//     await getTotalUsers();
-//     const { totalUsers } = await import('../tot_users');
-
-//     expect(totalUsers).toBe(4);
-//   });
-
-//   it('sets totalUsers to 0 on failed response (ok=false)', async () => {
-//     fetch.mockResolvedValueOnce({ ok: false });
-
-//     await getTotalUsers();
-//     const { totalUsers } = await import('../tot_users');
-
-//     expect(totalUsers).toBe(0);
-//   });
-
-//   it('sets totalUsers to 0 on network error', async () => {
-//     fetch.mockRejectedValueOnce(new Error('Network error'));
-
-//     await getTotalUsers();
-//     const { totalUsers } = await import('../tot_users');
-
-//     expect(totalUsers).toBe(0);
-//   });
-// });
 
 
 
