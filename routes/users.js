b8ff3c6097ccef2e6b5aafffbcc3afe1929bca09
Kanizo11 <kananelorampele7@gmail.com>
2025-05-24 23:34:31 +0200

    update weather api

--- routes/users.js
+++ routes/users.js
@@ -295,7 +295,7 @@ const createUserRoutes = (db, admin) => {
   });
 
 
-  cron.schedule('00 21 * * *', async () => {
+  cron.schedule('01 00 * * *', async () => {
   try {
     const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${LOCATION.lat}&lon=${LOCATION.lon}&exclude=current,minutely,hourly,alerts&units=metric&appid=${OPENWEATHER_API_KEY}`;
     const response = await fetch(url);
