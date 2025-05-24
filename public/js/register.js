5f84c41abdba6abeb366859bb0e748751a702313
strangerhm3@gmail.com <strangerhm3@gmail.com>
2025-05-24 05:21:54 +0200

    remove all security concerns

--- public/js/register.js
+++ public/js/register.js
@@ -2,3 +2,8 @@ import { googleSignIn } from './googlesignIn.js';
 
 
 document.getElementById("googleSignInButton").addEventListener("click", googleSignIn);
+btnGoLog
+document.getElementById("btnGoLog").addEventListener("click",async(e)=>{
+    e.preventDefault();
+    window.location.href = "./login_page.html"; 
+});
